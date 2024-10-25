d620c8ff2983ec88cc4092375759d4f668bc68d3
Leung Cheng <monewckl@hotmail.com.hk>
2024-10-25 21:40:04 +0800

    Update product (Rebase this later)

--- src/main/java/com/leungcheng/spring_simple_backend/domain/Product.java
+++ src/main/java/com/leungcheng/spring_simple_backend/domain/Product.java
@@ -2,17 +2,23 @@ package com.leungcheng.spring_simple_backend.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "UUID")
     private String id;
 
+    @NotBlank
     private String name;
+
+    @Min(0)
     private double price;
+
+    @Min(0)
     private int quantity;
 
     public String getName() {
