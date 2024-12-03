8a344d1a2a33c146efae74c4966275afb5b61a74
Leung Cheng <monewckl@hotmail.com.hk>
2024-12-03 12:57:14 +0800

    Get user in orderService

--- src/main/java/com/leungcheng/spring_simple_backend/domain/order/OrderService.java
+++ src/main/java/com/leungcheng/spring_simple_backend/domain/order/OrderService.java
@@ -1,6 +1,7 @@
 package com.leungcheng.spring_simple_backend.domain.order;
 
 import com.leungcheng.spring_simple_backend.domain.ProductRepository;
+import com.leungcheng.spring_simple_backend.domain.User;
 import com.leungcheng.spring_simple_backend.domain.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,11 @@ public class OrderService {
   }
 
   public Order createOrder(String userId, PurchaseItems purchaseItems) {
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("User does not exist"));
+
     throw new UnsupportedOperationException("Not implemented");
   }
 }
